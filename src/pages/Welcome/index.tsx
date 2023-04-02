c58ec1fa67339d003d1a113d86ba587c72cfd943
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-04-02 14:54:21 +0200

    :hammer:

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,35 +1,5 @@
-import { Button, Fade, Typography, styled } from '@mui/material';
-
-const ButtonStyled = styled(Button)(({ theme }) => ({
-    width: '8rem',
-    height: '2.6rem',
-}));
-
-const ButtonContainerLang = styled('div')(({ theme }) => ({
-    width: '35rem',
-    height: '5rem',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-}));
-
-const ButtonContainer = styled('div')(({ theme }) => ({
-    width: '40rem',
-    height: '5rem',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-}));
-
-const WelcomeContainer = styled('div')(({ theme }) => ({
-    margin: 0,
-    width: 'auto',
-    height: '100vh',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-}));
+import { Fade, Typography } from '@mui/material';
+import { ButtonContainer, ButtonContainerLang, ButtonStyled, WelcomeContainer } from './styled';
 
 export function Welcome() {
     setTimeout(() => {
---
+++ src/pages/Welcome/styled.tsx
@@ -0,0 +1,32 @@
+import { Button, styled } from '@mui/material';
+
+export const ButtonStyled = styled(Button)(({ theme }) => ({
+    width: '8rem',
+    height: '2.2rem',
+}));
+
+export const ButtonContainerLang = styled('div')(({ theme }) => ({
+    width: '35rem',
+    height: '5rem',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+}));
+
+export const ButtonContainer = styled('div')(({ theme }) => ({
+    width: '40rem',
+    height: '5rem',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+}));
+
+export const WelcomeContainer = styled('div')(({ theme }) => ({
+    margin: 0,
+    width: 'auto',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+}));
