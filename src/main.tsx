f6fdd8b5477bd48bfac139df4c88ab7800f9184c
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-04-08 23:16:53 +0200

    Added package redux-persis :package:

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom/client';
-import { store } from './redux';
+import { Loading } from './pages/Loading';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux';
 import './i18n';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={<Loading />} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -8,14 +8,34 @@ import {
 import { useDispatch } from 'react-redux';
 import { themeReducer } from './theme';
 import { configReducer } from './config';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
 
 export const rootReducer = combineReducers({ config: configReducer, theme: themeReducer });
-export const store = configureStore({ reducer: rootReducer });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredPaths: ['items.dates'],
+                ignoredActions: ['your/action/type'],
+                ignoredActionPaths: ['register', 'rehydrate', 'persist/PERSIST'],
+            },
+        }),
+});
 
 const _getDispatch = () => store.dispatch;
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = ReturnType<typeof _getDispatch>;
 
+export const persistor = persistStore(store);
 export const useAppDispatch = (): ThunkDispatch<CombinedState<RootState>, undefined, AnyAction> =>
     useDispatch<AppDispatch>();
