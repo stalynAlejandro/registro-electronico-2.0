896e12c735c955066cf34f72cdea7e4cf1e53085
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-04-10 13:21:24 +0200

    :sparkles:

--- src/components/AuthCard/index.tsx
+++ src/components/AuthCard/index.tsx
@@ -7,7 +7,6 @@ import {
     TitleContainer,
     ActionsContainer,
 } from './styled';
-import MyIcon from './vite.svg';
 import { ArrowForward } from '@mui/icons-material';
 
 interface IAuthCard {
@@ -21,7 +20,7 @@ export const AuthCard = ({ title, info, infoUser }: IAuthCard) => {
         <CardContainer>
             <TitleContainer>
                 <CardTitle children={`${title}`} />
-                <CardLogo src={MyIcon} alt="vite-logo" />
+                <CardLogo src={'assets/vite.svg'} alt="vite-logo" />
             </TitleContainer>
 
             <InfoContainer>
@@ -30,11 +29,7 @@ export const AuthCard = ({ title, info, infoUser }: IAuthCard) => {
             </InfoContainer>
 
             <ActionsContainer>
-                <AuthButton
-                    variant="contained"
-                    children={`${title}`}
-                    endIcon={<ArrowForward />}
-                />
+                <AuthButton variant="contained" children={`${title}`} endIcon={<ArrowForward />} />
             </ActionsContainer>
         </CardContainer>
     );
