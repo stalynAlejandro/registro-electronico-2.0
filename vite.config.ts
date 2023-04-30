bdee365711c2f8fc390f3093b6802bf6cdefbec8
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-04-30 13:27:07 +0200

    :hammer:

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
@@ -5,4 +7,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        // setupFiles: './src/test/setup.ts',
+        coverage: {
+            provider: 'c8',
+            reporter: ['text', 'json', 'html'],
+        },
+    },
 });
