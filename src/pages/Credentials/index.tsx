a57ae8cd79bee7fab139d84c8e695538ef55c5fb
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-06-13 15:07:59 +0700

    :fire:

--- src/pages/Credentials/index.tsx
+++ src/pages/Credentials/index.tsx
@@ -1,22 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { Fade } from '@mui/material';
 
 export function Credentials() {
     const [result, setResult] = useState('Calculating....');
+    const [input2, setInput2] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
+    const handleInputChange2 = (event: { target: { value: React.SetStateAction<string> } }) => {
+        setInput2(event.target.value);
+    };
+
     const fn = () => {
+        const worker = new Worker('hellworker.ts');
         setResult('');
         setIsLoading(true);
-
-        const worker = new Worker('hellworker.ts');
         worker.postMessage(null);
-
         worker.onmessage = function (e) {
             setIsLoading(false);
             setResult(e.data);
+            worker.terminate();
         };
     };
 
@@ -26,6 +30,11 @@ export function Credentials() {
                 <Header />
                 <h2 style={{ marginTop: 90 }}>Result: {isLoading ? 'Loading' : result}</h2>
                 <button onClick={fn}>Start</button>
+                <div>
+                    <label>
+                        <input type="text" value={input2} onChange={handleInputChange2} />
+                    </label>
+                </div>
                 <Footer />
             </div>
         </Fade>
