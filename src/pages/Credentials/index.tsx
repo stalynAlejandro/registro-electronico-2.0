b2c808036dcafe50afe67c17359f842e98153204
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-07-31 13:56:05 +0200

    :hammer: :rocket:

--- src/pages/Credentials/index.tsx
+++ src/pages/Credentials/index.tsx
@@ -1,44 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react';
-
-let worker = new Worker('hellworker.ts');
-
-const WorkComponent = (props: { count: number; sendMessageToWorker: (arg0: number) => void }) => {
-    const { sendMessageToWorker, count } = props;
-
-    const handleSendMessageToWorker = () => {
-        sendMessageToWorker(count);
-    };
-
-    const handleResetWorker = () => {
-        sendMessageToWorker(1);
-    };
-
-    return (
-        <div>
-            <button onClick={handleSendMessageToWorker}>Click Worker</button>
-            <button onClick={handleResetWorker}>Reset</button>
-        </div>
-    );
-};
-
 export function Credentials() {
-    const [count, setCount] = useState(2);
-    const sendMessageToWorker = useCallback((data: number) => worker.postMessage(data), [worker]);
-
-    const onKillWorker = () => worker.terminate();
-
-    useEffect(() => {
-        worker.onmessage = (event: MessageEvent) => {
-            const { data } = event;
-            setCount(data);
-        };
-    }, [worker]);
-
     return (
         <div>
-            <span>{count}</span>
-            <WorkComponent count={count} sendMessageToWorker={sendMessageToWorker} />
-            <button onClick={() => onKillWorker()}>Kill Worker</button>
+
         </div>
     );
 }
